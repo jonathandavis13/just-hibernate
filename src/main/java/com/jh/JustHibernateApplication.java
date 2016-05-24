4988e6aac2c24640d5973f87194fb2a16637ba7e
Davis <jonathan.davis@intelligrated.com>
2016-05-24 13:15:10 -0400

    Create initial Spring Project

---
+++ src/main/java/com/jh/JustHibernateApplication.java
@@ -0,0 +1,12 @@
+package com.jh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustHibernateApplication.class, args);
+	}
+}
