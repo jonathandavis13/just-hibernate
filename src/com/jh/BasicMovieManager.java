d38304b6eb8335017a06d0b1814b67101f753a97
Davis <jonathan.davis@intelligrated.com>
2016-07-06 10:14:11 -0400

    create find movie functionality

--- src/com/jh/BasicMovieManager.java
+++ src/com/jh/BasicMovieManager.java
@@ -1,6 +1,8 @@
 package com.jh;
 
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -29,14 +31,39 @@ public class BasicMovieManager {
 		session.getTransaction().commit();
 	}
 	
+	private Movie findMovie(long movieId){
+		Session session = sessionFacotry.getCurrentSession();
+		session.beginTransaction();
+		Movie movie = (Movie) session.load(Movie.class, movieId);
+		session.getTransaction().commit();
+		return movie;
+	}
+	
+	private List<Movie> findAll(){
+		Session session = sessionFacotry.getCurrentSession();
+		session.beginTransaction();
+		List<Movie> movies = session.createQuery("from movie").list();
+		session.getTransaction().commit();
+		return movies;
+	}
+	
 	public static void main(String[] args){
 		BasicMovieManager basicMovieManager = new BasicMovieManager();
 		
-		Movie movie = new Movie();
-		movie.setDirector("Jon Favreau");
-		movie.setSynopsis("After being held captive in an Afghan cave, a billionaire engineer creates a unique weaponized suit of armor to fight evil.");
-		movie.setTitle("Iron Man");
-		basicMovieManager.persistMovie(movie);
+		Movie ironMan = new Movie();
+		ironMan.setDirector("Jon Favreau");
+		ironMan.setSynopsis("After being held captive in an Afghan cave, a billionaire engineer creates a unique weaponized suit of armor to fight evil.");
+		ironMan.setTitle("Iron Man");
+		basicMovieManager.persistMovie(ironMan);
+		
+		Movie manOfSteel = new Movie();
+		manOfSteel.setDirector("Zach Snyder");
+		manOfSteel.setTitle("Man of Steel");
+		manOfSteel.setSynopsis("Clark Kent, one of the last of an extinguished race disguised as an unremarkable human, is forced to reveal his identity when Earth is invaded by an army of survivors who threaten to bring the planet to the brink of destruction.");
+//		basicMovieManager.persistMovie(manOfSteel);
+		
+		System.out.println(String.format("MOVIE: %S", basicMovieManager.findMovie(0).getTitle()));
+		
 	}
 
 }
